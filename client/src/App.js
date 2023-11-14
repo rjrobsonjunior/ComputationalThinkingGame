9a9c59ac938d3e3dd2b0fa8df09511071e1daff2
Robson Junior <rob.santanaju@gmail.com>
2023-11-14 17:54:18 -0300

    go(gin) + React app init

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,23 @@
-import {Browser, BrowserRouter, Navigate, Route, Routes} from 'react-router-dom';
-import HomePage from './scenes/homePage'
-import LoginPage from './scenes/loginPage'
-import ProfilePage from './scenes/profilePage'
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path = "/" element={<LoginPage/>} />
-          <Route path = "/home" element={<HomePage/>} />
-          <Route path = "/profile/:userId" element={<ProfilePage/>} />
-        </Routes>
-      </BrowserRouter>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- client/src/scenes/profilePage/index.jsx
+++
@@ -1,9 +0,0 @@
-const profilePage = () => {
-    return (
-        <div>
-            profilePage
-        </div>
-    )
-}
-
-export default profilePage;
